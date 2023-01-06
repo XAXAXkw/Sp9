6ebca9955738d8a06aaa33471dab3720153fdd99
Ivan Cuadros <ivancuadros@gmail.com>
2023-01-07 00:31:00 +0100

    ReadMe

--- portfolio/src/components/BetaZone.vue
+++ portfolio/src/components/BetaZone.vue
@@ -1,16 +1,32 @@
 <template>
+    <br />
+    <br />
     <div>
 <h1>BetaZone</h1>
-<img class='icons' src="@/assets/icon_comics.png" />
-<p>comics</p>
-<img class='icons' src="@/assets/icon_pencil.png" />
-<p>drawings</p>
-<img class='icons' src="@/assets/icon_canvas.png" />
-<p>paintings</p>
-<img class='icons' src="@/assets/icon_sculpture.png" />
-<p>sculptures</p>
-<img class='icons' src="@/assets/icon_music.png" />
-<p>music</p>
+
+<hr />
+<br />
+<div class="container  bg-light  p-4">FULL CATALOGUE</div>
+<div class="iconer container d-flex p-4 ">
+<div><img class='icons' src="@/assets/icon_comics.png" />
+<p>comics</p></div>
+
+<div><img class='icons' src="@/assets/icon_pencil.png" />
+<p>drawings</p></div>
+
+<div><img class='icons' src="@/assets/icon_canvas.png" />
+<p>paintings</p></div>
+
+<div><img class='icons' src="@/assets/icon_sculpture.png" />
+<p>sculptures</p></div>
+
+<div><img class='icons' src="@/assets/icon_music.png" />
+<p>music</p></div>
+
+
+</div>
+
+
     </div>
 </template>
 
@@ -27,7 +43,14 @@
 
 <style lang="css" scoped>
 .icons{
-    width: 10vmax;
+    width: 75px;
     padding:10px;
+}
+p{
+    font-size: 6px;
+    color:black;
+}
+.iconer{
+    justify-content:space-between;
 }
 </style>
